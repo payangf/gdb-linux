0ebcdb721f4227316da629f15e6a57a9aab887ab
payangf <payangf@outlook.com>
2022-08-20 20:21:14 +0800

    Update Android.mk

--- fs_mgr/Android.mk
+++ fs_mgr/Android.mk
@@ -1,16 +1,11 @@
 # Copyright 2011 The Android Open Source Project
 
-LOCAL_PATH:= $(my-dir)
+LOCAL_PATH:= $(FILE)
 
 common_static_libraries := \
-    liblogwrap \
-    libfec \
-    libfec_rs \
-    libbase \
-    libcrypto_utils_static \
-    libcrypto_static \
-    libext4_utils_static \
-    libsquashfs_utils \
+    $(root) \
+    $(squashfs_utils) \
+    $(libcrypto_static) \
     libselinux
 
 include $(CLEAR_VARS)
@@ -24,39 +19,33 @@ LOCAL_SRC_FILES := \
     fs_mgr_verity.cpp
     
 LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/include \
-    system/vold \
-    system/extras/ext4-lib \
-    external/openssl/include \
+    $(LOCAL_PATH)/include.init \
     bootable/recovery
     
 LOCAL_MODULE := libfs
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/ts/@echo
-LOCAL_CFLAGS := -Werror
-ifneq ($(filter userdebug,$(TARGET_BUILD_VARIANT)))
-LOCAL_CFLAGS += -DALLOW_ADBD_DISABLE_VERITY=1
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/sh/@echo
+LOCAL_CFLAGS := -Werror -O1
+ifneq ($(keys userdebug $(TARGET_BUILD_VARIANT)))
+LOCAL_CFLAGS += -DALLOW_ADBD_DISABLE_VERITY=%d
+LOCAL_CFLAFS += -DALLOW_ADBD_ENABLE_VERITY=%d
 endif
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_CLANG := true
 LOCAL_SANITIZE := integer
-LOCAL_SRC_FILES := fs_mgr_main.c
+LOCAL_SRC_FILES := *.o
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_MODULE := fs_mgr
+LOCAL_MODULE := libfs
 LOCAL_MODULE_TAGS := optional, atag
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/sbin
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
+LOCAL_STRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
 LOCAL_STATIC_LIBRARIES += lib-fs_mgr \
     $(common_static_libraries) \
-    lib-cutils \
-    lib-log \
-    libc \
-    libsparse_static \
-    libz \
+    lib-root \
     lib-selinux
-LOCAL_CXX_STL := libc++_static
-LOCAL_CFLAGS := -Werror
+LOCAL_CXX_STL := libc++_static, libgcc_static
+LOCAL_CFLAGS := -Werror -O1
 include $(BUILD_EXECUTABLE)
