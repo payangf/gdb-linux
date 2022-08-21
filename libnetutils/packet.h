24b11f66ab6e6621ab3d0e6127931d83d6585055
payangf <payangf@outlook.com>
2022-08-21 18:05:59 +0800

    Update packet.h

--- libnetutils/packet.h
+++ libnetutils/packet.h
@@ -14,12 +14,14 @@
  * limitations under the License.
  */
 
-#ifndef _WIFI_PACKET_H_
-#define _WIFI_PACKET_H_
+#ifndef _WIFI_PACKET_H
+#define WIFI_PACKET_H (1)
 
-int open_raw_socket(const char *ifname, uint8_t *hwaddr, int if_index);
-int send_packet(int s, int if_index, struct dhcp_msg *msg, int size,
+#include "dhcpmsg.h"
+
+int open_raw_socket(const char *iname, uint8_t *hwaddr, int ifindex);
+int send_packet(int s, int ifindex, struct dhcp_msg __msg, int size,
                 uint32_t saddr, uint32_t daddr, uint32_t sport, uint32_t dport);
-int receive_packet(int s, struct dhcp_msg *msg);
+int receive_packet(int s, struct dhcp_msg);
 
-#endif
+#endif /* __WIFI_PACKET_H__ */
