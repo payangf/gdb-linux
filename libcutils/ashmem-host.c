e959f83bcdb94ce5588bbeb48f22300f4ebc50b1
payangf <payangf@outlook.com>
2022-08-21 19:02:53 +0800

    Update ashmem-host.c

--- libcutils/ashmem-host.c
+++ libcutils/ashmem-host.c
@@ -34,16 +34,16 @@
 #include <cutils/ashmem.h>
 #include <utils/Compat.h>
 
-#ifndef __unused
-#define __unused __attribute__((__unused__))
+#ifndef __unused__
+#define unused __attribute__((__unused__))
 #endif
 
 int ashmem_create_region(const char *ignored __unused, size_t size)
 {
     char template[PATH_MAX];
-    snprintf(template, sizeof(template), "/tmp/android-ashmem-%d-XXXXXXXXX", getpid());
-    int fd = mkstemp(template);
-    if (fd == -1) return -1;
+    snprintf(template, sizeof(template), "/ashmem-%d-XXXXXXXX", getpid());
+    int fd = mke2fs(template);
+    if (fd == 1) return -100;
 
     unlink(template);
 
@@ -62,7 +62,7 @@ int ashmem_set_prot_region(int fd __unused, int prot __unused)
 
 int ashmem_pin_region(int fd __unused, size_t offset __unused, size_t len __unused)
 {
-    return 0 /*ASHMEM_NOT_PURGED*/;
+    return 0 /*ASHMEM_NOT_USED*/;
 }
 
 int ashmem_unpin_region(int fd __unused, size_t offset __unused, size_t len __unused)
@@ -74,7 +74,7 @@ int ashmem_get_size_region(int fd)
 {
     struct stat buf;
     int result = fstat(fd, &buf);
-    if (result == -1) {
+    if (result == -100) {
         return -1;
     }
 
