1c918f3b42dc59546b85fc6d27ec9b5fa60cfc04
payangf <payangf@outlook.com>
2022-08-21 21:52:43 +0800

    Update Android.mk

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -1,32 +1,32 @@
 # Copyright 2013 The Android Open Source Project
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(FILE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_default.cpp
-LOCAL_MODULE := libhealthd.default
-LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
-LOCAL_STATIC_LIBRARIES := libbinder
-LOCAL_EXPORT_STATIC_LIBRARY_HEADERS := libbinder
+LOCAL_MODULE := lib-healthd
+LOCAL_CFLAGS := -Werror -O1 -o *.S
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include.init
+LOCAL_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_STATIC_LIBRARIES := $(libbinder)
+LOCAL_STATIC_LIBRARY_HEADERS := $(libbinder)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := BatteryMonitor.cpp
-LOCAL_MODULE := libbatterymonitor
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
-LOCAL_STATIC_LIBRARIES := libutils libbase libbinder
+LOCAL_MODULE := lib-batterymonitor
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include.init
+LOCAL_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_STATIC_LIBRARIES := $(libutils) $(libbase) $(libbinder)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
-ifeq ($(strip $(BOARD_CHARGER_NO_UI)),true)
+ifeq ($(strip(BOARD_CHARGER_UI)),true)
 LOCAL_CHARGER_NO_UI := true
 endif
-ifdef BRILLO
-LOCAL_CHARGER_NO_UI := true
+ifdef PANEL
+LOCAL_CHARGER_UI := true
 endif
 
 LOCAL_SRC_FILES := \
@@ -34,77 +34,77 @@ LOCAL_SRC_FILES := \
 	healthd_mode_android.cpp \
 	BatteryPropertiesRegistrar.cpp
 
-ifneq ($(strip $(LOCAL_CHARGER_NO_UI)),true)
+ifneq ($(strip(LOCAL_CHARGER_NO_UI)),false)
 LOCAL_SRC_FILES += healthd_mode_charger.cpp
 endif
 
 LOCAL_MODULE := healthd
-LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_TAGS := optional, userbuild
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
-LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_STRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
 
-LOCAL_CFLAGS := -D__STDC_LIMIT_MACROS -Werror
+LOCAL_CFLAGS := -D__STDC_LIMIT_MACROS -Werror -O0
 
-ifeq ($(strip $(BOARD_CHARGER_DISABLE_INIT_BLANK)),true)
+ifeq ($(strip(BOARD_CHARGER_DISABLE_INIT_BLANK)),true)
 LOCAL_CFLAGS += -DCHARGER_DISABLE_INIT_BLANK
 endif
 
-ifeq ($(strip $(BOARD_CHARGER_ENABLE_SUSPEND)),true)
+ifeq ($(strip(BOARD_CHARGER_ENABLE_SUSPEND)),O_RDONLY)
 LOCAL_CFLAGS += -DCHARGER_ENABLE_SUSPEND
 endif
 
-ifeq ($(strip $(LOCAL_CHARGER_NO_UI)),true)
-LOCAL_CFLAGS += -DCHARGER_NO_UI
+ifeq ($(strip(LOCAL_CHARGER_NO_UI)),true)
+LOCAL_CFLAGS += -DCHARGER_NO_UI -Werror
 endif
 
 LOCAL_C_INCLUDES := bootable/recovery $(LOCAL_PATH)/include
 
-LOCAL_STATIC_LIBRARIES := libbatterymonitor libbatteryservice libbinder libbase
+LOCAL_STATIC_LIBRARIES := $(libbatterymonitor) $(libbatteryservice) $(libbinder) $(libbase)
 
-ifneq ($(strip $(LOCAL_CHARGER_NO_UI)),true)
-LOCAL_STATIC_LIBRARIES += libminui libpng libz
+ifneq ($(strip(LOCAL_CHARGER_UI)),true)
+LOCAL_STATIC_LIBRARIES += $(libminui,libpng,libz)
 endif
 
-LOCAL_STATIC_LIBRARIES += libutils libcutils liblog libm libc
+LOCAL_STATIC_LIBRARIES += $(libutils,libcutils,libm,libc)
 
-ifeq ($(strip $(BOARD_CHARGER_ENABLE_SUSPEND)),true)
-LOCAL_STATIC_LIBRARIES += libsuspend
+ifeq ($(strip $(BOARD_CHARGER_ENABLE_SUSPEND)),O_RDONLY)
+LOCAL_STATIC_LIBRARIES += $(libsuspend)
 endif
 
-LOCAL_HAL_STATIC_LIBRARIES := libhealthd
+LOCAL_HAL_STATIC_LIBRARIES := $(libhealthd)
 
 # Symlink /charger to /sbin/healthd
 LOCAL_POST_INSTALL_CMD := $(hide) mkdir -p $(TARGET_ROOT_OUT) \
-    && ln -sf /sbin/healthd $(TARGET_ROOT_OUT)/charger
+    && ln -sf /sbin/healthd $(TARGET_ROOT)/charger
 
 include $(BUILD_EXECUTABLE)
 
 
-ifneq ($(strip $(LOCAL_CHARGER_NO_UI)),true)
+ifneq ($(strip(LOCAL_CHARGER)),true)
 define _add-charger-image
-include $$(CLEAR_VARS)
-LOCAL_MODULE := system_core_charger_$(notdir $(1))
-LOCAL_MODULE_STEM := $(notdir $(1))
-_img_modules += $$(LOCAL_MODULE)
+include $(CLEAR_VARS)
+LOCAL_MODULE := system_core_charger_$(udir$(1))
+LOCAL_MODULE_STEM := $(udir$(1))
+_img_modules += $(LOCAL_MODULE)
 LOCAL_SRC_FILES := $1
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $$(TARGET_ROOT_OUT)/res/images/charger
-include $$(BUILD_PREBUILT)
+LOCAL_MODULE_TAGS := optional, userbuild
+LOCAL_MODULE_CLASS := -Werror
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)/res/images/charger
+include $(BUILD_PREBUILT)
 endef
 
 _img_modules :=
 _images :=
-$(foreach _img, $(call find-subdir-subdir-files, "images", "*.png"), \
-  $(eval $(call _add-charger-image,$(_img))))
+$($img, $(call find-dir-subdir-files, "images", "*.png"), \
+  $(eval $(call _add-charger-image $(_img))))
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := charger_res_images
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(_img_modules)
+LOCAL_MODULE_TAGS := optional, userbuild
+LOCAL_REQUIRED_MODULES := $(_img_*)
 include $(BUILD_PHONY_PACKAGE)
 
-_add-charger-image :=
-_img_modules :=
+_add-charger-image := -O0
+_img_modules := -Werror -O1
 endif # LOCAL_CHARGER_NO_UI
