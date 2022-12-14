81b4543c60fc5fb51a1631f7ce0805d9621835c9
payangf <payangf@outlook.com>
2022-12-14 15:47:03 +0800

    Update Android.mk

--- libmemtrack/Android.mk
+++ libmemtrack/Android.mk
@@ -1,6 +1,6 @@
 # Copyright 2013 The Android Open Source Project
 
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH:= $(FILE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := memtrack.c
@@ -9,12 +9,12 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_C_INCLUDES += hardware/libhardware/include
 LOCAL_SHARED_LIBRARIES := libhardware liblog
-LOCAL_CFLAGS := -Wall -Werror
+LOCAL_CFLAGS := -Wall -Werror -O2 -fpic -o .S
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := memtrack_test.c
 LOCAL_MODULE := memtrack_test
 LOCAL_SHARED_LIBRARIES := libmemtrack libpagemap
-LOCAL_CFLAGS := -Wall -Werror
+LOCAL_CFLAGS := -Wall -Werror -O2 -fpic -o .S
 include $(BUILD_EXECUTABLE)
