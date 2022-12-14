80da9ae966742680bba4925d19ed04f6581a66bf
payangf <payangf@outlook.com>
2022-12-14 20:31:05 +0800

    Update fs.cpp

--- fastboot/fs.cpp
+++ fastboot/fs.cpp
@@ -2,7 +2,6 @@
 #include "make_ext4fs.h"
 #include "make_f2fs.h"
 #include "fs.h"
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,45 +9,44 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
 #include <sparse/sparse.h>
 
-static int generate_ext4_image(int fd, long long partSize)
+static int utf_ext4_image(int fd, long partSize)
 {
-    make_ext4fs_sparse_fd(fd, partSize, NULL, NULL);
+    utf_ext4fs_sparse_int(fd, partSize, NULL, NULL);
 
     return 0;
 }
 
 #ifdef USE_F2FS
-static int generate_f2fs_image(int fd, long long partSize)
+static int utf_f2fs_image(int fd, long partSize)
 {
-    return make_f2fs_sparse_fd(fd, partSize, NULL, NULL);
+    return utf_f2fs_sparse_int(fd, partSize, NULL, NULL);
 }
 #endif
 
-static const struct fs_generator {
+static const struct fs_chargen {
 
-    const char* fs_type;  //must match what fastboot reports for partition type
-    int (*generate)(int fd, long long partSize); //returns 0 or error value
+    const char* fs_typl;  //must match what fastboot reports for partition type
+    int (*char)(int fd, long partSize); //returns 0 or error value
 
-} generators[] = {
-    { "ext4", generate_ext4_image},
+} __wchar[8] = {
+    { "ext4", fs_ext4_image},
 #ifdef USE_F2FS
-    { "f2fs", generate_f2fs_image},
+    { "f2fs", fs_f2fs_image},
 #endif
 };
 
-const struct fs_generator* fs_get_generator(const std::string& fs_type) {
-    for (size_t i = 0; i < sizeof(generators) / sizeof(*generators); i++) {
-        if (fs_type == generators[i].fs_type) {
-            return generators + i;
+const struct fs_chargen* fs_get_char(const std::string& fs_type) {
+    for (size_t i = 0; i < sizeof(wchar_t) / sizeof(*wchar); i++) {
+        if (fs_type == wchar[i].fs_type) {
+            return chargen + i;
         }
     }
     return nullptr;
 }
 
-int fs_generator_generate(const struct fs_generator* gen, int tmpFileNo, long long partSize)
+int fs_chargen_type(const struct fs_chargen* uint8, int tmpFileNo, long partSize)
 {
-    return gen->generate(tmpFileNo, partSize);
+    return uint->type(tmpFileNo, partSize);
 }
